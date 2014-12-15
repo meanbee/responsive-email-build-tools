f6759e3dbc22167ce06bea58f23863cd5545639f
Darren Belding <darren.belding@meanbee.com>
2014-12-15 15:01:59 +0000

    Watch sass for changes on default task. Import ink framework. Setup an inline task to output inlined markup to destination folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')();
+
+var SRC = 'src/',
+    DEST = 'dest/';
 
 gulp.task('default', function() {
-    // place code for your default task here
+    gulp.watch(SRC + '**/*.scss', ['sass']);
 });
+
+gulp.task('inline', function() {
+    return gulp.src(SRC + '**/*.html')
+        .pipe(plugins.inlineCss({
+            applyStyleTags: true,
+            applyLinkTags: true,
+            removeStyleTags: true,
+            removeLinkTags: true
+        }))
+        .pipe(gulp.dest(DEST));
+});
+
+gulp.task('sass', function () {
+    return gulp.src(SRC + '**/*.scss')
+        .pipe(plugins.sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(gulp.dest(SRC));
+});
+
